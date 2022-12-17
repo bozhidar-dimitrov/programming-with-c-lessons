a7b6196848fb501ef962d6d77be73fbcef4a06c8
Bozhidar Dimitrov <bozhidar.dimitrov@gmail.com>
2022-12-17 11:32:55 +0200

    Renaming folder name and fixing some code

--- bitwise-operators/9a/bitmasks.c
+++ bitwise-operators/9a/bitmasks.c
@@ -5,9 +5,9 @@ int main(void) {
   //0000 0000
   unsigned char rooms = 0;
 
-  unsigned char room1 = 1 << 0;
-  unsigned char room2 = 1 << 1;
-  unsigned char room3 = 1 << 2;
+  unsigned char room1 = 1 << 0; //0000 0001
+  unsigned char room2 = 1 << 1; //0000 0010
+  unsigned char room3 = 1 << 2; //0000 0100
   unsigned char room4 = 1 << 3;
   unsigned char room5 = 1 << 4;
   unsigned char room6 = 1 << 5;
@@ -15,9 +15,9 @@ int main(void) {
   unsigned char room8 = 1 << 7;
 
   //Turns the light in room 4
-  //0100 1001
-  //0000 1000
-  //| 0100 1001
+  //  0000 0000
+  //  0000 1000
+  //| 0000 1000
   rooms = rooms | room4;
 
   int roomnum = 6;
@@ -25,13 +25,13 @@ int main(void) {
   rooms = rooms | 1 << (roomnum - 1);
 
   //0101 1101
-  //0000 1000 
-  //& 0000 1000
+  //0000 0010 
+  //& 0000 0000
   //Checks whether the lights is on in room 4
-  printf("Is lights in room 4 turned on?: %d", rooms & room4 == room4);
+  printf("Is lights in room 4 turned on?: %d", (rooms & room2) == room2);
 
   //Checks whether the light is turned on in roomnum
-  rooms & (1 << (roomnum - 1)) == 1 << (roomnum - 1);
+  (rooms & (1 << (roomnum - 1))) == 1 << (roomnum - 1);
 
   return 0;
 }
