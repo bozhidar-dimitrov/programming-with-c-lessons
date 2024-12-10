3ea01f623146e882c0477d2f32d38e9922e4c8d7
Bozhidar Dimitrov <bozhidar.dimitrov@gmail.com>
2024-12-10 10:23:21 +0200

    Bit masks 9a, 9b

--- 9-typecasting-and-bitwise-operators/9a/bitMasks.c
+++ 9-typecasting-and-bitwise-operators/9a/bitMasks.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+void printLightsState(unsigned char state) {
+  int stateBits = sizeof(state) * 8;
+  for (int i = stateBits- 1; i >= 0; i--) {
+    printf("Light %d - %u\n", i + 1, (state >> i) & 1);
+  }
+  printf("\n");
+}
+
 int main(void) {
 
   unsigned char lights = 0; //0000 0000
@@ -12,6 +20,61 @@ int main(void) {
   unsigned char light6 = 1 << 5; //0010 0000
   unsigned char light7 = 1 << 6; //0100 0000
   unsigned char light8 = 1 << 7; //1000 0000
+
+  printLightsState(lights);
+
+  //Turn on light4, light6, light1
+
+  //0000 0000 - lights
+  //0000 1000 - light4
+  //0000 1000 - lgihts | lights4
+  lights = lights | light4;
+
+  lights |= light6;
+  lights |= light1;
+  printf("After turning on lights: \n");
+  printLightsState(lights);
+
+  //Check if a light is turned on:
+  
+  //0010 0001 - lights
+  //0000 1000 - mask of light 4
+  //0000 1000
+  //if (light & light4 != 0)
+  if (lights & light4) {
+    printf("Light 4 is turned on!\n");
+  }
+
+  //Check if light 6 is turned on
+  if ((lights >> 5) & 1) {
+    printf("Light 6 is turned on!\n");
+  }
+
+  //Turn off the light if it is turned on:
+
+  printf("Turn off light 6:");
+  //Turn off light 6
+  //0010 1001 - lights
+  //0010 0000 - light6
+  //1101 1111 - ~light6
+  //0000 1001 - lights & (~light6)
+  lights &= ~light6;
+  printLightsState(lights);
+
+  //0010 1001 - lights
+  //0000 1000 - light4
+
+  //0010 0001 - lights ^ light4
+  //0000 1000 - light4
+  //0010 1001 - lights ^ light4
+  //=> XOR siwtches the light (if the light is turned off it will turn it on and if it is turned on it will turned it on)
+  printf("First XOR on light4\n");
+  lights ^= light4;
+  printLightsState(lights);
+
+  printf("Second XOR on light4\n");
+  lights ^= light4;
+  printLightsState(lights);
 
   return 0;
 }
--- 9-typecasting-and-bitwise-operators/9b/bitMasks.c
+++ 9-typecasting-and-bitwise-operators/9b/bitMasks.c
@@ -17,6 +17,9 @@ int main(void) {
 
   lights = lights | light4; //Turn on the fourth light
 
+  lights = lights | light6; //Turn on the sixth light
+
+
   for (int i = sizeof(lights) * 8 - 1; i >= 0; i--) {
     printf("%u", (lights >> i) & 1);
   }
@@ -29,9 +32,51 @@ int main(void) {
   //0000 1000 (light4)
   //0000 1001 (lights)
   //0000 1000 lights & light4 == (light4)
-  if ( light4 = lights & light4) {
+  if ( light4 == (lights & light4)) {
     printf("Light 4 is on\n");
   }
+
+  if ( (lights & light4) != 0) {
+    printf("Light 4 is on\n");
+  }
+
+  //0000 1001 - lights
+  //0000 1000 - light4
+  //0000 0001 - desired state
+
+  //Turns off light 4 
+  if ((lights >> 3) & 1) {
+    lights -= light4;
+  }
+
+  for (int i = sizeof(lights) * 8 - 1; i >= 0; i--) {
+    printf("%u", (lights >> i) & 1);
+  }
+  printf("\n");
+
+  //Turn off light1 if it is turned on, or turn the light1 if it is turned off
+  lights = lights ^ light1;
+
+  for (int i = sizeof(lights) * 8 - 1; i >= 0; i--) {
+    printf("%u", (lights >> i) & 1);
+  }
+  printf("\n");
+
+
+  //0010 1001 - original lights value
+  //0010 0000 - light6
+
+  //1101 1111 - ~light6
+  //0010 1001 - 
+  //0000 1001 - lights & (~light6)
+
+  //Turn off light 6. If it is turned off it stays turned off
+  lights = lights & (~light6);
+
+  for (int i = sizeof(lights) * 8 - 1; i >= 0; i--) {
+    printf("%u", (lights >> i) & 1);
+  }
+  printf("\n");
 
   return 0;
 }
