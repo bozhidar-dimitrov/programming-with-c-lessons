1416533f3931bc5484f5106ba2ef0effee1ef68a
Bozhidar Dimitrov <bozhidar.dimitrov@gmail.com>
2026-04-01 18:34:04 +0300

    Dynamic Array frame completed for all classes

--- 17-dynamic-array/9b/DynamicArray.c
+++ 17-dynamic-array/9b/DynamicArray.c
@@ -19,8 +19,38 @@ DynamicArray init(uint capacity) {
   return dynArr;
 }
 
+static void resize(DynamicArray * dynArr, uint newSize){
+  uint oldCapacity = dynArr->capacity;
+  if (oldCapacity < newSize) {
+    if (dynArr->capacity == 0) {
+      dynArr->capacity = 1;
+    }
+    while (dynArr->capacity < newSize) {
+      dynArr->capacity *= 2;
+    }
+  } if (newSize <= oldCapacity / 2) {
+    dynArr->capacity = newSize;
+  }
+
+  if (dynArr->capacity != oldCapacity) {
+    if (dynArr->capacity == 0) {
+      free(dynArr->buffer);
+      dynArr->buffer == NULL;
+    } else {
+      DynArrType * temp = realloc(dynArr->buffer, sizeof(DynArrType) * dynArr->capacity);
+      ASSERT_ALLOC(temp);
+      dynArr->buffer = temp;
+    }
+  }
+
+  dynArr->size = newSize;
+}
+
 void pushBack(DynamicArray * dynArr, DynArrType value) {
-  
+  const uint oldSize = dynArr->size;
+  const uint newSize = oldSize + 1;
+  resize(dynArr, newSize);
+  dynArr->buffer[newSize-1] = value;
 }
 
 void pushFront(DynamicArray * dynArr, DynArrType value) {
@@ -28,11 +58,17 @@ void pushFront(DynamicArray * dynArr, DynArrType value) {
 }
 
 void push(DynamicArray * dynArr, uint index, DynArrType value) {
-
+  
 }
 
 DynArrType popBack(DynamicArray * dynArr) {
-  return 0;
+  const uint oldSize = dynArr->size;
+  const uint newSize = oldSize - 1;
+
+  DynArrType lastElement = dynArr->buffer[oldSize - 1];
+  resize(dynArr, newSize);
+
+  return lastElement;
 }
 
 DynArrType popFront(DynamicArray * dynArr) {
--- 17-dynamic-array/9b/tests.c
+++ 17-dynamic-array/9b/tests.c
@@ -40,6 +40,39 @@ void testPushBackMethod() {
     && dynArr.buffer[1] == 10 
     && dynArr.buffer[2] == 15
   );
+
+  ASSERT_TEST(
+    "When pushing more items than the initial capacity, the capacity of the dynamic array should be doubled",
+    dynArr.capacity == initialCapacity * 2
+  );
+}
+
+void testPopBackMethod() {
+  int initialCapacity = 2;
+  DynamicArray dynArr = init(initialCapacity);
+
+  pushBack(&dynArr, 5);
+  pushBack(&dynArr, 10);
+  pushBack(&dynArr, 15);
+
+  DynArrType lastElement = popBack(&dynArr);
+  ASSERT_TEST("After pushing some elements in the dynamic array and calling the popBack method once, the method should return the last element",
+    lastElement == 15
+  );
+
+  ASSERT_TEST("After pushing three elements and then popping one, the capacity of the dynamic array should become 2",
+    dynArr.capacity == 2
+  );
+
+  DynArrType secondToLast = popBack(&dynArr);
+  ASSERT_TEST("After pushing some elements in the dynamic array and calling the popBack method twice, the method should return the second to last element",
+    secondToLast == 10
+  );
+
+  ASSERT_TEST("After pushing three elements and then popping two, the capacity of the dynamic array should become 1",
+    dynArr.capacity == 1
+  );
+
 }
 
 int main(void) {
@@ -47,7 +80,7 @@ int main(void) {
   dynamicArrayWithCapacityGreaterThanZeroShouldHaveBufferInitialezed();
   dynamicArrayWithCapacityEqualZeroShouldHaveNULLBuffer();
   testPushBackMethod();
-
+  testPopBackMethod();
 
   return 0;
 }
